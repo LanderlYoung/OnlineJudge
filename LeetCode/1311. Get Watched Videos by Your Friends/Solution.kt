a8cfc213786450e397999ba477a11a081a503687
landerlyoung <landerlyoung@gmail.com>
2024-07-04 17:15:19 +0800

    AC

---
+++ LeetCode/1311. Get Watched Videos by Your Friends/Solution.kt
@@ -0,0 +1,77 @@
+package problem1311
+
+import matrix2d
+import shouldBeEqualTo
+
+class Solution {
+  fun watchedVideosByFriends(
+    watchedVideos: List<List<String>>,
+    friends: Array<IntArray>,
+    id: Int,
+    level: Int,
+  ): List<String> {
+    val visited = BooleanArray(friends.size)
+
+    var l = 0
+    val queue = ArrayDeque<Int>()
+    queue.add(id)
+    visited[id] = true
+
+    while (l < level) {
+      val size = queue.size
+      repeat(size) {
+        val f = queue.removeFirst()
+        for (ff in friends[f]) {
+          if (!visited[ff]) {
+            visited[ff] = true
+            queue.add(ff)
+          }
+        }
+
+        l++
+      }
+    }
+
+    val wordsCount = HashMap<String, Int>()
+    queue.forEach { f ->
+      watchedVideos[f].forEach {
+        val prev = wordsCount.get(it)
+        wordsCount[it] = prev?.let { it + 1 } ?: 0
+      }
+    }
+
+    return wordsCount.entries.sortedWith(
+      compareBy<Map.Entry<String, Int>> { it.value }.thenBy { it.key }
+    ).map { it.key }
+  }
+}
+
+fun main() {
+  fun test(
+    watchedVideos: String, friends: String, id: Int, level: Int,
+    expected: List<String>,
+  ) {
+    val wv: List<List<String>> =
+      watchedVideos.trim('[', ']').split("],[").map {
+        it.split(',').map { it.trim('"') }
+      }
+
+    val result = Solution().watchedVideosByFriends(wv, matrix2d(friends), id, level)
+    println("$ -> ${result} == ${expected} -> ${result == expected}")
+    result shouldBeEqualTo expected
+  }
+
+  test(
+    """[["A","B"],["C"],["B","C"],["D"]]""",
+    "[[1,2],[0,3],[0,3],[1,2]]",
+    0, 1,
+    listOf("B", "C")
+  )
+
+  test(
+    """[["A","B"],["C"],["B","C"],["D"]]""",
+    "[[1,2],[0,3],[0,3],[1,2]]",
+    0, 2,
+    listOf("D")
+  )
+}
